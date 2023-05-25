6b4e2b915c3b57fd074ac068db79f58725079bee
FirestormPenguin <edoardogiubellini@gmail.com>
2023-05-25 19:00:57 +0200

    commit

--- so_long.c
+++ so_long.c
@@ -6,18 +6,50 @@
 /*   By: egiubell <egiubell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 17:09:43 by egiubell          #+#    #+#             */
-/*   Updated: 2023/05/25 17:11:06 by egiubell         ###   ########.fr       */
+/*   Updated: 2023/05/25 19:00:49 by egiubell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
 int	main()
 {
 	void	*mlx;
 	void	*mlx_win;
+	t_data	img;
+	int i;
+	int y;
+	int x;
 
+	i = 50;
+	y = 480;
+	x = 640;
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_win = mlx_new_window(mlx, x, y, "Finestra!");
+	img.img = mlx_new_image(mlx, x, y);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	while (i < x - 50)
+	{
+		my_mlx_pixel_put(&img, i, 50, 0x00FF00FF);
+		i++;
+	}
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
