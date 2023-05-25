d1e4a3705f3b02f702f2cacc555875bf3052f482
FirestormPenguin <edoardogiubellini@gmail.com>
2023-05-25 20:29:45 +0200

    commit

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: egiubell <egiubell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 17:09:43 by egiubell          #+#    #+#             */
-/*   Updated: 2023/05/25 19:01:11 by egiubell         ###   ########.fr       */
+/*   Updated: 2023/05/25 20:29:38 by egiubell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,11 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
+
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
@@ -28,28 +33,34 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
+int	close_win(void *mlx, void *win)
+{
+	mlx_destroy_window(mlx, win);
+	return (0);
+}
+
 int	main()
 {
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
+	t_data	*data;
+	t_vars vars;
 	int i;
 	int y;
 	int x;
 
+	data = (t_data *)malloc(sizeof(t_data));
 	i = 50;
 	y = 480;
 	x = 640;
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, x, y, "Finestra!");
-	img.img = mlx_new_image(mlx, x, y);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
+	vars.mlx = mlx_init();		//inizia mlx
+	vars.win = mlx_new_window(vars.mlx, x, y, "Finestra!");		//genera finestra
+	data->img = mlx_new_image(vars.mlx, x, y);		//genera immagine
+	data->addr = mlx_get_data_addr(data->img, &data->bits_per_pixel, &data->line_length, &data->endian);		//gestice in maniera corretta dimensione e posizione dei pixel
 	while (i < x - 50)
 	{
-		my_mlx_pixel_put(&img, i, 50, 0x00FF00FF);
+		my_mlx_pixel_put(data, i, 50, 0x00FF00FF);		//genera pixel nell'immmagine
 		i++;
 	}
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	mlx_put_image_to_window(vars.mlx, vars.win, data->img, 0, 0);		//put immagine nella finestra
+	mlx_hook(vars.win, 2, 1L<<0, close_win, &vars);			//trigger evento chiusura finestra alla pressione di un tasto
+	mlx_loop(vars.mlx);			//gestione loop finestra tenuta attiva
 }
