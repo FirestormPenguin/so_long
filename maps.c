4365aad25aa3646dabd5459946b89c0eabe1bbd8
FirestormPenguin <edoardogiubellini@gmail.com>
2023-05-29 20:19:48 +0200

    commit

--- maps.c
+++ maps.c
@@ -6,7 +6,7 @@
 /*   By: egiubell <egiubell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 18:13:49 by egiubell          #+#    #+#             */
-/*   Updated: 2023/05/29 19:31:16 by egiubell         ###   ########.fr       */
+/*   Updated: 2023/05/29 20:17:07 by egiubell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,18 @@ int count_col(char *path)
 	int fd;
 		
 	i = 0;
-	str = malloc(sizeof(char) * 100);
+	str = malloc(sizeof(char) * 50000);
 	fd = open(path, O_RDONLY);
 	str = get_next_line(fd);
-	close (fd);
 	while (str[i] != '\n')
 	{
 		i++;
 	}
+	while (str)
+		str = get_next_line(fd);
+	close (fd);
 	free (str);
-	return (i - 1);
+	return (i);
 }
 
 int count_line(char *path)
@@ -38,7 +40,7 @@ int count_line(char *path)
 	int fd;
 		
 	i = 0;
-	str = malloc (sizeof(char) * 100);
+	str = malloc (sizeof(char) * 50000);
 	fd = open(path, O_RDONLY);
 	while (str)
 	{
@@ -55,23 +57,22 @@ int get_map(char *path, t_vars *vars)
 	int i;
 	int fd;
 	char *str;
-	int line;
-	//int col;
 
 	i = 0;
-	line = count_line(path);
-	//col = count_col(path);
+	vars->line = count_line(path);
+	vars->column = count_col(path);
 	fd = open(path, O_RDWR);
 	str = malloc (sizeof(char) * 100);
-	vars->map = malloc (sizeof(char *) * line);
-	while (i < line)
+	vars->map = malloc (sizeof(char *) * vars->line);
+	while (i < vars->line)
 	{
 		str = get_next_line(fd);
-		vars->map[i] = malloc(sizeof(char *) * 100);
+		vars->map[i] = malloc(sizeof(char *) * vars->column);
 		vars->map[i] = str;
 		printf("%s", vars->map[i]);
 		i++;
 	}
+	printf("\nLines: %d\nColumns: %d\n", vars->line, vars->column);
 	free(str);
 	close (fd);
 	return (0);
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: egiubell <egiubell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 17:06:40 by egiubell          #+#    #+#             */
-/*   Updated: 2023/05/29 18:53:45 by egiubell         ###   ########.fr       */
+/*   Updated: 2023/05/29 20:00:43 by egiubell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,11 @@
 
 typedef struct s_vars {
 	char **map;
+	int line;
+	int column;
+	int collect;
+	int exit;
+	int player;
 }			t_vars;
 
 int get_map(char *path, t_vars *vars);
