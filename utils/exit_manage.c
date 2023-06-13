026c0eacc1050513f17d082fb6b863818b9e9908
FirestormPenguin <edoardogiubellini@gmail.com>
2023-06-13 16:06:24 +0200

    commit

--- utils/exit_manage.c
+++ utils/exit_manage.c
@@ -6,7 +6,7 @@
 /*   By: egiubell <egiubell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 19:58:19 by egiubell          #+#    #+#             */
-/*   Updated: 2023/06/12 18:55:29 by egiubell         ###   ########.fr       */
+/*   Updated: 2023/06/13 15:58:55 by egiubell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,14 @@ void	close_generic(t_graph *graph)
 {
 	mlx_destroy_window(graph->mlx, graph->win);
 	free(graph->mlx);
+	free(graph->img_terrain);
+	free(graph->img_wall);
+	free(graph->img_player);
+	free(graph->img_exit);
+	free(graph->img_collect);
+	free(graph->img_monster);
+	free(graph->img_monster2);
+	free(graph->img_ammo);	
 }
 
 int	close_winning(t_game *game)
